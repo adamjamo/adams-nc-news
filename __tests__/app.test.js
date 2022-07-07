904d96944cb80c40cd10a9cf78c4d424aa7c5406
Adam James <adamjames411@gmail.com>
2022-07-07 09:51:46 +0100

    deleted console log

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -140,7 +140,6 @@ describe("GET /api/articles/:article_id/comments", () => {
         .get(`/api/articles/${article_id}/comments`)
         .expect(200)
         .then(({ body }) => {
-          console.log(body, "IS THIS HERE?????");
           expect(body.comments).toEqual([
             {
               article_id: 3,
