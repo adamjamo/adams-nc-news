3e94b1a593f320c9022c7c8924b21ffc828ed126
Adam James <adamjames411@gmail.com>
2022-07-08 10:50:39 +0100

    ticket 10 - posts a new comment to the database based on article ID

--- db/errors.js
+++ db/errors.js
@@ -6,23 +6,43 @@ exports.customError = (err, req, res, next) => {
   } else next(err);
 };
 
-exports.stringInsteadOfNum = (err, req, res, next) => {
-  if (err.code === "22P02") {
-    res.status(400).send({ message: "Invalid data type, must be a number!" });
-  } else next(err);
-};
-
 exports.emptyPatch = (err, req, res, next) => {
-  console.log(err);
-  if (err.code === "23502") {
+  if (err.code === "23502" && Object.keys(req.body).length === 0) {
     res.status(400).send({ message: "Empty input" });
   } else next(err);
 };
 
-exports.invalidPath = (req, res) => {
+exports.failingSchema = (err, req, res, next) => {
+  if (err.code === "23502" && Object.keys(req.body).length > 1) {
+    res
+      .status(400)
+      .send({ message: "failing Schema, please check your inputs" });
+  } else next(err);
+};
+
+exports.noArticleId = (err, req, res, next) => {
+  if (err.code === "23503" && err.constraint === "comments_article_id_fkey") {
+    res.status(404).send({ message: "Article ID not found!" });
+  } else next(err);
+};
+
+exports.noUser = (err, req, res, next) => {
+  if (err.code === "23503" && err.constraint === "comments_author_fkey") {
+    res.status(404).send({ message: "Username does not exist" });
+  } else next(err);
+};
+
+exports.stringInsteadOfNum = (err, req, res, next) => {
+  if (err.code === "22P02" || err.code === "42601") {
+    console.log(err, "STRINGINSTEADOFNUM");
+    res.status(400).send({ message: "Invalid data type, must be a number!" });
+  } else next(err);
+};
+
+exports.invalidPath = (req, res, next) => {
   res.status(404).send({ message: "Path not found" });
 };
 
-exports.serverErr = (err, req, res, next) => {
-  res.status(500).send({ message: "Server error" });
+exports.serverError = (err, req, res, next) => {
+  res.status(500).send({ message: "internal server error" });
 };
