be13b7e9f734bc92dbfd0ce244b29dc23fb6cb70
Adam James <adamjames411@gmail.com>
2022-07-22 14:36:06 +0100

    linked listen to app

--- db/tableOutput.sql
+++ db/tableOutput.sql
@@ -1,3 +1,6 @@
 \c nc_news
 
-SELECT articles.* , COUNT(comments.article_id) AS comment_count FROM articles LEFT JOIN comments on comments.article_id = articles.article_id GROUP BY articles.article_id
+SELECT articles.* , 
+COUNT(comments.article_id) AS comment_count FROM articles
+LEFT JOIN comments on comments.article_id = articles.article_id 
+GROUP BY articles.article_id
